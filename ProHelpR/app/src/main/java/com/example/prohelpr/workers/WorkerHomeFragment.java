028b3b433e78f4d0eebb3593ddf39392204b98b1
keerthi <keerthireddy932@gmail.com>
2021-05-04 13:05:11 +0530

    recyclerview in worker home and bookings

--- ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerHomeFragment.java
+++ ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerHomeFragment.java
@@ -3,64 +3,30 @@ package com.example.prohelpr.workers;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.prohelpr.R;
+import com.example.prohelpr.Worker_Home_data;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WorkerHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WorkerHomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView rv;
 
-    public WorkerHomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WorkerHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WorkerHomeFragment newInstance(String param1, String param2) {
-        WorkerHomeFragment fragment = new WorkerHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    String[] data = {"MyData","MyData"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_worker_home, container, false);
+        View v =  inflater.inflate(R.layout.fragment_worker_home, container, false);
+        rv = v.findViewById(R.id.w_h_recycler);
+        rv.setAdapter(new Worker_Home_data(getContext(),data));
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        return v;
     }
 }
--- ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerProfileFragment.java
+++ ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerProfileFragment.java
@@ -13,19 +13,36 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.prohelpr.R;
 import com.example.prohelpr.auth.MainActivity;
+import com.example.prohelpr.models.Worker;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
 
 
 public class WorkerProfileFragment extends Fragment {
     String phoneNumber,roledata;
-    TextView role,mobileNumber,workername,workeraddress,workerplace,workercategory,potsalcode,saveworker;
+    TextView role,mobileNumber,savingstatus;
+
+    TextInputEditText workerrole,workername,workernumber,workeraddress,workerplace,workermail,workerpostalcode,workeramount;
+    MaterialButton saveworker;
+    Spinner workercategory,workerstatus;
+
+    List<Worker> workers;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,14 +57,30 @@ public class WorkerProfileFragment extends Fragment {
         mobileNumber = v.findViewById(R.id.mobileNumber);
         role = v.findViewById(R.id.roledefine);
         role.setText("Ypur Role Is : "+roledata);
-
         mobileNumber.setText(phoneNumber);
+
+        /*Getting ID*/
+        workerrole = v.findViewById(R.id.role_name);
         workername=v.findViewById(R.id.worker_name);
+        workernumber=v.findViewById(R.id.worker_number);
+        workermail=v.findViewById(R.id.worker_mail);
         workeraddress=v.findViewById(R.id.worker_address);
         workerplace=v.findViewById(R.id.worker_place);
         workercategory=v.findViewById(R.id.worker_category);
-        potsalcode=v.findViewById(R.id.worker_postalcode);
+        workerstatus =v.findViewById(R.id.worker_status);
+        workerpostalcode=v.findViewById(R.id.worker_postalcode);
+        workeramount=v.findViewById(R.id.worker_amount);
+
         saveworker=v.findViewById(R.id.save_worker);
+        savingstatus = v.findViewById(R.id.savingstatus);
+
+        workernumber.setText(phoneNumber);
+        workerrole.setText(roledata);
+
+        workers = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Worker");
 
         v.findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,7 +96,19 @@ public class WorkerProfileFragment extends Fragment {
         v.findViewById(R.id.save_worker).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Worker Data Update Sucessfully", Toast.LENGTH_SHORT).show();
+
+                String w_role = workerrole.getText().toString();
+                String w_name = workername.getText().toString();
+                String w_number =workernumber.getText().toString();
+                String w_mail =workermail.getText().toString();
+                String w_address =workeraddress.getText().toString();
+                String w_place= workerplace.getText().toString();
+                String w_pcode=workerpostalcode.getText().toString();
+                String w_category=workercategory.getSelectedItem().toString();
+                String w_staus=workerstatus.getSelectedItem().toString();
+                String w_amount=workeramount.getText().toString();
+
+                Toast.makeText(getContext(), ""+w_role+w_name+w_number+w_mail+w_address+w_place+w_pcode+w_category+w_staus, Toast.LENGTH_SHORT).show();
             }
         });
 
