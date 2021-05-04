99084f4202b7a8f310f0fc757bfe699a1f6d85a4
Giri30-09-98 <giri30091998@gmail.com>
2021-05-04 12:40:19 +0530

    WorkersDatabase

--- ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerProfileFragment.java
+++ ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerProfileFragment.java
@@ -4,28 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.prohelpr.R;
 import com.example.prohelpr.auth.MainActivity;
+import com.example.prohelpr.workersProfiles.WorkersDatabase;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
 
 
 public class WorkerProfileFragment extends Fragment {
     String phoneNumber,roledata;
-    TextView role,mobileNumber,workername,workeraddress,workerplace,workercategory,potsalcode,saveworker,workermobilenumber;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    WorkersDatabase workersDatabase;
+    List<WorkersDatabase> list;
+    TextView role,mobileNumber,workername,workeraddress,workerplace,workercategory,saveworker,workermobilenumber;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,14 +46,17 @@ public class WorkerProfileFragment extends Fragment {
         roledata = prefs.getString("role",NULL);
         mobileNumber = v.findViewById(R.id.mobileNumber);
         role = v.findViewById(R.id.roledefine);
-        role.setText("Ypur Role Is : "+roledata);
+        role.setText("Your Role Is : "+roledata);
 
         mobileNumber.setText(phoneNumber);
         workername=v.findViewById(R.id.worker_name);
         workeraddress=v.findViewById(R.id.worker_address);
         workerplace=v.findViewById(R.id.worker_place);
         workercategory=v.findViewById(R.id.worker_category);
-        potsalcode=v.findViewById(R.id.worker_postalcode);
+        list=new ArrayList<>();
+        database=FirebaseDatabase.getInstance();
+        reference=database.getReference("WorkersDatabase");
+
         saveworker=v.findViewById(R.id.save_worker);
         workermobilenumber=v.findViewById(R.id.worker_mobilenumber);
 
@@ -64,7 +74,22 @@ public class WorkerProfileFragment extends Fragment {
         v.findViewById(R.id.save_worker).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Worker Data Update Sucessfully", Toast.LENGTH_SHORT).show();
+                String name=workername.getText().toString();
+                String roles=role.getText().toString();
+                String contact=mobileNumber.getText().toString();
+                String address=workeraddress.getText().toString();
+                String category=workercategory.getText().toString();
+                String place=workerplace.getText().toString();
+
+                WorkersDatabase workersDatabase=new WorkersDatabase(name,roles,contact,address,category,place);
+                list.add(workersDatabase);
+                reference.push().setValue(list).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getActivity(), "Worker Data Update Sucessfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         });
 
